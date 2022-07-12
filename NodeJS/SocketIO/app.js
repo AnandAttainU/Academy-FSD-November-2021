17558168f7fccc201c224d90eacd4f6c7233c804
Anand AttainU <anand.didwania@attainu.com>
2022-07-12 23:11:15 +0530

    refactoring

--- NodeJS/SocketIO/app.js
+++ NodeJS/SocketIO/app.js
@@ -4,27 +4,21 @@ const { Server } = require("socket.io");
 
 const app = express();
 const httpServer = createServer(app);
+// Creating the socket server
 const socketServer = new Server(httpServer);
 
 app.use(express.static('public'));
 
+// handler for client connecting to server
 socketServer.on('connection', (connectionObject)=>{
     console.log("client has connected");
     console.log(connectionObject.id);
 
+    // event handler for custom event sendData
     connectionObject.on('sendData', (msg)=>{
         if(msg){
-            switch(msg.receiver){
-                case "999":
-                    connectionObject.broadcast.emit('broadcastMessage', msg.text);
-                    break;
-                case "100":
-                    connectionObject.broadcast.emit('broadcastMessage', "This data is private");
-                    break;
-                default:
-                    connectionObject.broadcast.emit('broadcastMessage', "You can not send data");
-            }
-            
+            // broadcasting message to all the other client except self
+            connectionObject.broadcast.emit('broadcastMessage', msg.text);
         }
     })
 })
--- NodeJS/SocketIO/public/index.js
+++ NodeJS/SocketIO/public/index.js
@@ -1,27 +1,30 @@
+// creating socket object
 const socket = io();
 
+// connecting to the socket server
 socket.on('connect', ()=>{
     console.log("Connected to server");
 })
 
+// All the html elements
 const inputEle = document.getElementById('inputTag');
 const buttonEle = document.getElementById('sendButton');
-const receiverEle = document.getElementById('receiver');
 const myMsg = document.getElementById('chatMessage');
 const broadcastMsg = document.getElementById('broadcastMsg');
 
 buttonEle.addEventListener('click', ()=>{
+    // creating the input object
     const data = {
-        text: inputEle.value,
-        receiver: receiverEle.value
+        text: inputEle.value
     }
 
     myMsg.innerText = data.text;
-
+    // sending the input data to custom event sendData
     socket.emit('sendData', data);
 
 })
 
+// handling the broadcasted message
 socket.on('broadcastMessage', (data)=>{
     broadcastMsg.innerText += data;
 })
